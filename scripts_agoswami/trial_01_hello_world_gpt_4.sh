dd2b1334ff6a3a3a5d4c69db16355faf9327b1e8
Abhishek Goswami <abgoswam@gmail.com>
2025-02-19 17:29:35 -0800

    scripts for hello_world using gpt-4o and claude-3-5-sonnet-20241022

---
+++ scripts_agoswami/trial_01_hello_world_gpt_4.sh
@@ -0,0 +1,5 @@
+sweagent run \
+  --agent.model.name=gpt-4o \
+  --agent.model.per_instance_cost_limit=2.00 \
+  --env.repo.github_url=https://github.com/abgoswam/py_repo_01 \
+  --problem_statement.github_url=https://github.com/abgoswam/py_repo_01/issues/1
---
+++ scripts_agoswami/trial_02_hello_world_claude.sh
@@ -0,0 +1,5 @@
+sweagent run \
+  --agent.model.name=claude-3-5-sonnet-20241022 \
+  --agent.model.per_instance_cost_limit=2.00 \
+  --env.repo.github_url=https://github.com/abgoswam/py_repo_01 \
+  --problem_statement.github_url=https://github.com/abgoswam/py_repo_01/issues/1
